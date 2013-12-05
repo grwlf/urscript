ec2b542620bf6f060c8251f7a87773495e8fbed2
Sergey Mironov <grrwlf@gmail.com>
2013-12-06 02:53:31 +0400

    Improve the buildsystem

--- Cakefile.hs
+++ Cakefile.hs
@@ -23,13 +23,17 @@ project = do
     module_ (single "Test1.ur")
 
   rule $ do
+    phony "run"
+    shell [cmd|$t|]
+
+  rule $ do
+    phony "clean"
+    unsafeShell [cmd|rm -rf .cake3 $(tempfiles u) $(tempfiles t)|]
+
+  rule $ do
     phony "all"
     depend u
     depend t
-
-  rule $ do
-    phony "run"
-    shell [cmd|$t|]
 
   return ()
 
